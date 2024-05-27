9cd455db0ab24bd9422ac76f452470d8a3b3266a
Kuvrot <jamessanz@outlook.com>
2024-05-26 18:01:15 -0600

    improvements in about page

--- about.php
+++ about.php
@@ -1,84 +1,137 @@
-  
-<?php
 
- session_start();
-         
-          $count=0;
-        if (isset($_SESSION['cart']))
-         {
-          $count=count($_SESSION['cart']);
-      
-          # code...
-        }
-?>
   
 
+ <?php
 
-  <!------------------made with love......-->
-
-  <!DOCTYPE html>
-  <html>
-  <head>
-
-  	<html lang="en">
-	<link rel="icon" href="img\th (1).jfif" height="20%"/>
-	<title>Index page</title>
-	<!-- for-mobile-apps -->
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-     <meta name="keywords" content="online Vegetables and fruits,online Vegetables and fruits chennai,online Vegetables and fruits shopping,online Vegetables and fruits store,online supermarket,Vegetables and fruits chennai, buy Vegetables and fruits online,online Vegetables and fruits purchase,online vegetable store"/>
-    <meta name="description" content="Buy fresh Vegetables and fruits at online store of odisha-India. Online Supermarket includes fresh vegetable,fruits,greens. online Vegetables and fruits Shopping now made easy. Order Vegetables and fruits online with door step delivery all over Chennai at the best price & quality with get free home delivery."/>
-
+session_start();
+        
+         $count=0;
+       if (isset($_SESSION['cart']))
+        {
+         $count=count($_SESSION['cart']);
      
+         # code...
+       }
+?>
+ <!------------------made with love......-->
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+
+   <html lang="en">
+ <link rel="icon" href="img\th (1).jfif" height="20%"/>
+ <title>Tecnoldeliciosas</title>
+ <!-- for-mobile-apps -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <meta name="keywords" content="online Vegetables and fruits,online Vegetables and fruits chennai,online Vegetables and fruits shopping,online Vegetables and fruits store,online supermarket,Vegetables and fruits chennai, buy Vegetables and fruits online,online Vegetables and fruits purchase,online vegetable store"/>
+   <meta name="description" content="Buy fresh Vegetables and fruits at online store of odisha-India. Online Supermarket includes fresh vegetable,fruits,greens."/>
+
+    
 <!-- //for-mobile-apps -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 
-   <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
-     <!-- bootystrap java script---->
-   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-   
-   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <!-- bootystrap java script---->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
-   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>  
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>  
 
-    <script src="https://kit.fontawesome.com/67f58695eb.js" crossorigin="anonymous"></script>
+   <script src="https://kit.fontawesome.com/67f58695eb.js" crossorigin="anonymous"></script>
 
-    <!----coustum styles sheet--->
-      <link rel="stylesheet" type="text/css" href="css/styles.css">
-  </head>
-  <body>
+   <!----coustum styles sheet--->
+     <link rel="stylesheet" href="css/styles.css" >
+ </head>
+ <body>
+   <!--------------------navbar....================------------m_c_1_4_3-------------->
 
-  <?php include "parts/header.php"?>
+ <?php include "parts/header.php"?>
 
 <!--end of nav---mc_-->
 
-  	<div class="container-fluid about expand" style="color:white;">
-      <div class="container sub_about">
-        <h3>SOBRE NOSOTROS</h3>
-         <h4 style="text-align:center;">Misión</h4>
-         <p style="text-align:left;">
-          En Tecnodeliciosas Fresas, nuestra misión es proporcionar experiencias gastronómicas excepcionales a nuestros clientes, fusionando la frescura y el sabor de las fresas con la eficiencia y comodidad de las herramientas tecnológicas. Nos comprometemos a ofrecer productos de alta calidad, como fresas con crema, bolis y fresas con chocolate, utilizando procesos eficientes y sostenibles. Nos esforzamos por superar las expectativas de nuestros clientes, brindando un servicio excepcional y una experiencia de compra conveniente a través de nuestras plataformas electrónicas.
-        </p>
-        <br>
-        <h4 style="text-align:center;">Visión</h4>
-         <p style="text-align:left;">
-          Nuestra visión en Tecnodeliciosas Fresas es ser líderes en el sector de negocios electrónicos, destacando por nuestra innovación, calidad y atención al cliente. Nos visualizamos como una empresa reconocida a nivel regional por nuestra capacidad para integrar de manera efectiva las tecnologías de información y comunicación en todos los aspectos de nuestro negocio, desde la producción hasta la entrega. Aspiramos a ser pioneros en la implementación de sistemas ERP y CRM para optimizar nuestras operaciones y mejorar la experiencia del cliente. Con un enfoque en la excelencia y la constante mejora, nos esforzamos por ser un referente en la industria, estableciendo nuevos estándares de calidad y servicio.
-          </p>
-
-         </div>
-      
-    </div>
-
-    <!---footer part--->
 
 
-    <?php include "parts/footer.php";?>
+
+<div >
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+ <ol class="carousel-indicators">
+   <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+   <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+   <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+   <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+ </ol>
+ <div class="carousel-inner" style="height:50%">
+   <div class="carousel-item active" >
+     <img class="d-block w-100" src="img/about/mision.jpg" alt="First slide">
+   </div>
+   <div class="carousel-item">
+     <img class="d-block w-100" src="img/about/vision.jpg" alt="Second slide">
+   </div>
+   <div class="carousel-item">
+     <img class="d-block w-100" src="img/about/Valores.jpg" alt="Third slide">
+   </div>
+   <div class="carousel-item">
+     <img class="d-block w-100" src="img/about/OrganigramaPrincipal.png" alt="Third slide">
+   </div>
+   <div class="carousel-item">
+     <img class="d-block w-100" src="img/about/arturo.png" alt="Third slide">
+   </div>
+   <div class="carousel-item">
+     <img class="d-block w-100" src="img/about/meli.png" alt="Third slide">
+   </div>
+   <div class="carousel-item">
+     <img class="d-block w-100" src="img/about/yum.png" alt="Third slide">
+   </div>
+ </div>
+ <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+   <span class="sr-only">Previous</span>
+ </a>
+ <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+   <span class="carousel-control-next-icon" aria-hidden="true"></span>
+   <span class="sr-only">Next</span>
+ </a>
+</div>
+</div>
+
+
+</div>
+
+
+   <?php include "parts/footer.php";?>
+     <script>
+    
+
+
+   //pageup.....
+
+   var mybutton = document.getElementById('top');
   
-  </body>
-  </html>
+   window.onscroll = function() 
+   {scrollFunction()};
 
+function scrollFunction()
+{
+ if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+   mybutton.style.display = "block";
+ } else {
+   mybutton.style.display = "none";
+ }
+}
 
+// When the user clicks on the button, scroll to the top of the document
+function pageup()
+{
+ document.body.scrollTop = 0;
+ document.documentElement.scrollTop = 0;
+}
+   </script>
+ </body>
+ </html>
 
--- admin.php
+++ admin.php
@@ -32,7 +32,7 @@ height: 100%;
           class="img-fluid" alt="Sample image">
       </div>
       <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        <form>
+        <form action="admin.php" method="post">
           <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
             <p class="lead fw-normal mb-0 me-3">Admin panel</p>
             <button  type="button" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-floating mx-1">
@@ -51,14 +51,14 @@ height: 100%;
 
           <!-- Email input -->
           <div data-mdb-input-init class="form-outline mb-4">
-            <input type="text" id="form3Example3" class="form-control form-control-lg"
+            <input type="text" id="form3Example3" name="username" class="form-control form-control-lg"
               placeholder="Usuario..." />
             <label class="form-label" for="form3Example3">Usuario</label>
           </div>
 
           <!-- Password input -->
           <div data-mdb-input-init class="form-outline mb-3">
-            <input type="password" id="form3Example4" class="form-control form-control-lg"
+            <input type="password" name="password" id="form3Example4" class="form-control form-control-lg"
               placeholder="Contraseña..." />
             <label class="form-label" for="form3Example4">Contraseña</label>
           </div>
@@ -114,3 +114,16 @@ height: 100%;
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
+
+<?php 
+
+if (isset($_POST["username"])){
+    $adminname = $_POST["username"];
+    if ($adminname == "admin" && $_POST["password"] == "admin"){
+
+        $_SESSION["admin"] = $adminname;
+    }
+}
+
+?>
